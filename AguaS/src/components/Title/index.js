83874dba03a67c5df28021c00a1cde67adbebdc0
Enrico Carrano <28276248+EnricoEng@users.noreply.github.com>
2022-05-21 22:14:43 -0300

    Estruturando código react native

---
+++ AguaS/src/components/Title/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text} from "react-native"
+
+
+export default function Title() {
+    return (
+
+
+        <View>
+            <Text>AguaS</Text>
+        </View>
+
+
+    )
+}
