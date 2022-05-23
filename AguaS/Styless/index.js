b66c4de234648955f9775fad2721dd8a11003223
Enrico Carrano <28276248+EnricoEng@users.noreply.github.com>
2022-05-22 22:37:17 -0300

    alterações de estilo e estudo de como realizar a navegação do app

--- AguaS/Styless/index.js
+++ AguaS/Styless/index.js
@@ -4,13 +4,21 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create(
 
     {
+        //no titulo colocar background color = #000022
         container: {
-            flex: 1,
-            backgroundColor: '#ffff',
+            flex: 1, 
+            backgroundColor: '#000022',
             alignItems: 'center',
             justifyContent: 'center',
         },
-
+        startMainMenu: {
+            backgroundColor: '#001E6F',
+            flex: 1,
+            padding: 120,
+            marginTop: 20,
+            
+        },
+        
 
     }
 
--- AguaS/src/components/Main/index.js
+++ AguaS/src/components/Main/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { View, Text, Button} from 'react-native'
-
+import Styless from '../../../Styless'
 
 export default function Main() {
 
 
     return (
-        <View>
+        <View style={Styless.startMainMenu}>
             <Button title='Tirar foto do mês atual'></Button>
             <Button title='Monitor'></Button>
             <Button title='Histórico de fotos'></Button>
+            <Text> hauiehiuahuiehuiahueiai</Text>
         </View>
     )
 
